368b922070597c91cd61841aed24705a180fdbbb
alikarami010 <40549540+alikarami010@users.noreply.github.com>
2018-10-02 15:13:51 +0330

    Revert "Initial commit"
    This reverts commit ee324c6e88d2e0fea4b3ff2c43332d18ba7e9136.

--- app/src/main/java/nihad/uninews/ir/uninews/CommonClasses/Links.java
+++
@@ -1,9 +0,0 @@
-package nihad.uninews.ir.uninews.CommonClasses;
-
-public class Links {
-    private String WebServices = "http://PaymanYar.ir/PaymanWeb.asmx";
-
-    public String getWebServices() {
-        return this.WebServices;
-    }
-}
--- app/src/main/java/nihad/uninews/ir/uninews/CommonClasses/Setting.java
+++
@@ -1,9 +0,0 @@
-package nihad.uninews.ir.uninews.CommonClasses;
-
-public class Setting {
-    public static int AppVersion;
-    public static int DaramadAccId = 1;
-    public static int ExpireDate;
-    public static int HazinehAccId = 6;
-    public static int dbVersion;
-}
